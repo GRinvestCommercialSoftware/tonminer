b79c6575f6bd09ec694c55da3a66c14f378cec01
GRinvest <grfx@live.ru>
2024-02-10 01:18:24 +0700

    Create givers_pow.js

---
+++ givers_pow.js
@@ -0,0 +1,27 @@
+"use strict";
+// export const givers100 = [
+//     { address: 'EQCfwe95AJDfKuAoP1fBtu-un1yE7Mov-9BXaFM3lrJZwqg_', reward: 100 },
+//     { address: 'EQBoATvbIa9vA7y8EUQE4tlsrrt0EhSUK4mndp49V0z7Me3M', reward: 100 },
+//     { address: 'EQAV3tsPXau3VJanBw4KCFaMk3l_n3sX8NHZNgICFrR-9EGE', reward: 100 },
+//     { address: 'EQAR9DvLZMHo9FAVMHI1vHvL7Fi7jWgjKtUARZ2S_nopQRYz', reward: 100 },
+//     { address: 'EQC10L__G2SeEeM2Lw9osGyYxhoIPqJwE-8Pe7728JcmnJzW', reward: 100 },
+//     { address: 'EQDZJFkh12kw-zLGqKSGVDf1V2PRzedGZDFDcFml5_0QerST', reward: 100 },
+//     { address: 'EQCiLN0gEiZqthGy-dKl4pi4kqWJWjRzR3Jv4jmPOtQHveDN', reward: 100 },
+//     { address: 'EQDB8Mo9EviBkg_BxfNv6C2LO_foJRXcgEF41pmQvMvnB9Jn', reward: 100 },
+//     { address: 'EQAidDzp6v4oe-vKFWvsV8MQzY-4VaeUFnGM3ImrKIJUIid9', reward: 100 },
+//     { address: 'EQAFaPmLLhXveHcw3AYIGDlHbGAbfQWlH45WGf4K4D6DNZxY', reward: 100 }, // 100
+// ]
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.givers100000 = void 0;
+exports.givers100000 = [
+    { address: 'EQBM8I1p5PEJSgQQqKDaOxuuQzHFp7GLcBd5ChKNaQ159g6S', reward: 100000 },
+    { address: 'EQCBB90ecx2p29nMJQgRDMc8dpe5wJwfcoRjUMiALDQNlqWz', reward: 100000 },
+    { address: 'EQBEzPiNQlUK0HSHgFnjBq-HDA5xk5_JBAAGudvevPlKGnah', reward: 100000 },
+    { address: 'EQDuRwowi_uFUPk_M9wwGev7YRWK1TrTTp5RpuzZqYVjWeSw', reward: 100000 },
+    { address: 'EQC2udTftic-QAXcsy-GTbiRQqMyEXlhwRL9PFDJZbVQpZfS', reward: 100000 },
+    { address: 'EQBbXnon2BymX5QaJ4c-wBiqCQLufYx7QGkz2mqwM43SZHmI', reward: 100000 },
+    { address: 'EQAEDPdzi4bSb3fBwqWzI88sgkLeOa42hOgesxzeIfbJPtYP', reward: 100000 },
+    { address: 'EQDoMW51I0vaQisLJpi2Kbj8zaN79TgyYM2PnyXr-x80Er5_', reward: 100000 },
+    { address: 'EQCHKWlqed_fOFoZ-TjuhpAXrPE8yg2jnE63gxpZDt8v8WzJ', reward: 100000 },
+    { address: 'EQCTzNRQgFqfTa20MBvepeRpOzzBmnEWwVCII4rEqQw1ufzr', reward: 100000 }, // 100 000
+];
